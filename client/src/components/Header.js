92902fc39003768621a684a363030c54e1e1b34b
mdquayle <misternopants@gmail.com>
2022-01-30 12:31:32 -0500

    adds login button to Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,35 @@
-function Header() {
-    return(
-    <header className="header">
-        Movie Review Website!
+import { Link } from "react-router-dom";
+
+function Header({ user, onLogout }) {
+  function handleLogout() {
+    fetch("/logout", {
+      method: "DELETE",
+    }).then(() => onLogout());
+  }
+
+  return (
+    <header>
+      <h1>
+        Welcome To MoviReVew
+      </h1>
+      {user ? (
+        <div>
+          <p>Welcome {user.username}!</p>
+          <Link to="/">
+          <button onClick={handleLogout}>Logout</button>
+          </Link>
+        </div>
+      ) : (
+          <div>
+            <Link to="/login">
+                <button>Click Here to Login</button>
+            </Link>
+            <p>Welcome to MoviRevew </p>
+            <p>Please login to write reviews!</p>
+        </div>
+      )}
     </header>
-    )
+  );
 }
 
 export default Header;
