4aff976db8a16fe907e21132f25f6bd65a7afe86
mdquayle <misternopants@gmail.com>
2022-02-04 14:02:30 -0500

    changed way onLogin prop  was passed to LogIn component

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import {useNavigate} from 'react-router-dom';
 import SignUp from "./SignUp";
 
-function LogIn(onLogin){
+function LogIn({onLogin}){
         const [username, setUsername] = useState("");
         const [password, setPassword] = useState("");;
         const navigate = useNavigate()
